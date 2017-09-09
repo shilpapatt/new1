b183040e1d8adaabaeb40be7b2a782687428bb5f
unknown <shilpa66p@gmail.com>
2017-09-09 18:53:36 +0530

    2nd time push

--- src/main/java/com/org/test/MavenFirstProject/SignUp.java
+++ src/main/java/com/org/test/MavenFirstProject/SignUp.java
@@ -16,11 +16,11 @@ public class SignUp {
 			public void signup() {
 		  //  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		    driver.findElement(By.xpath("//a[text()='Sign Up Free']")).click();
-			driver.findElement(By.xpath("//input[@id='name'][@placeholder='Your name']")).sendKeys("S1dw");
-			driver.findElement(By.xpath("//*[@id='email'][@placeholder='Your email']")).sendKeys("S1dw@gmail.com");
-			driver.findElement(By.id("mobile")).sendKeys("9347690782");
+			driver.findElement(By.xpath("//input[@id='name'][@placeholder='Your name']")).sendKeys("sdsdf");
+			driver.findElement(By.xpath("//*[@id='email'][@placeholder='Your email']")).sendKeys("sddd@gmail.com");
+			driver.findElement(By.id("mobile")).sendKeys("9347690790");
 			driver.findElement(By.id("sub_btn_signup")).click();  
-		    driver.findElement(By.xpath("//input[@id='otp']")).sendKeys("123456");
+		    driver.findElement(By.xpath("//input[@id='otp']")).sendKeys("1244");
 			driver.findElement(By.cssSelector("button#sub_btn_verify")).click();
 		    driver.findElement(By.cssSelector("button.confirm")).click();
 	        }
