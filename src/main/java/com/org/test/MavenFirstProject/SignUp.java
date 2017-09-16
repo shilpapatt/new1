fcf4c58d190576cc670cc53d91672c96b24e6574
unknown <shilpa66p@gmail.com>
2017-09-16 16:22:59 +0530

    change

--- src/main/java/com/org/test/MavenFirstProject/SignUp.java
+++ src/main/java/com/org/test/MavenFirstProject/SignUp.java
@@ -11,13 +11,12 @@ public class SignUp {
 	        public SignUp(WebDriver driver2) 
 	        {
 		    driver=driver2;
-		    }
-			
+		    }	
 			public void signup() {
 		  //  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		    driver.findElement(By.xpath("//a[text()='Sign Up Free']")).click();
-			driver.findElement(By.xpath("//input[@id='name'][@placeholder='Your name']")).sendKeys("11alishaPat");
-			driver.findElement(By.xpath("//*[@id='email'][@placeholder='Your email']")).sendKeys("alisha@gmail.com");
+			driver.findElement(By.xpath("//input[@id='name'][@placeholder='Your name']")).sendKeys("shilpaPatt");
+			driver.findElement(By.xpath("//*[@id='email'][@placeholder='Your email']")).sendKeys("shilpaalisha@gmail.com");
 			driver.findElement(By.id("mobile")).sendKeys("9347690777");
 			driver.findElement(By.id("sub_btn_signup")).click();  
 		    driver.findElement(By.xpath("//input[@id='otp']")).sendKeys("1244");
